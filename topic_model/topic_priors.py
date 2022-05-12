5987156eb00b496e27610bde6b62765e875f726e
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-05-12 17:09:07 +0200

    fix: preserve order of topid_ids in txt file

--- topic_model/topic_priors.py
+++ topic_model/topic_priors.py
@@ -3,12 +3,20 @@ Topic priors from csv to Mallet format
 TODO: use spacy(?) to add all inflexions
 '''
 import pandas as pd
+import argparse
 
-filename = "topic_model/data/topic_priors_1.csv"
-df = pd.read_csv(filename)
+def main(args):
+	df = pd.read_csv(args.filename)
+	df = df.sort_values(by=['topic_id', 'topic', 'theme', 'word']).reset_index(drop=True)
 
-f = open(filename.replace('csv', 'txt'), 'w')
-for i, topic in enumerate(sorted(set(df['topic']))):
-	line = ', '.join(df.loc[df['topic'] == topic, 'word'].tolist())
-	f.write(f'{i}, ' + line + '\n')
+	f = open(args.filename.replace('csv', 'txt'), 'w')
+	for i in sorted(set(df['topic_id'])):
+		line = ', '.join(df.loc[df['topic_id'] == i, 'word'].tolist())
+		f.write(f'{i}, ' + line + '\n')
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument("--filename", type=str, default="topic_model/data/topic_priors_1.csv")
+	args = parser.parse_args()
+	main(args)
 
