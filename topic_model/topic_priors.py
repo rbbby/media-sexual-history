c2c68a251c0c713b497955d46d28d8f91e9c31fe
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-06-15 12:37:42 +0200

    fix: remove duplicate topic prior words

--- topic_model/topic_priors.py
+++ topic_model/topic_priors.py
@@ -6,17 +6,20 @@ import pandas as pd
 import argparse
 
 def main(args):
-	df = pd.read_csv(args.filename)
+	df = pd.read_csv(args.filename, sep=';')
+	
 	df = df.sort_values(by=['topic_id', 'topic', 'theme', 'word']).reset_index(drop=True)
-
 	f = open(args.filename.replace('csv', 'txt'), 'w')
 	for i in sorted(set(df['topic_id'])):
-		line = ', '.join(df.loc[df['topic_id'] == i, 'word'].tolist())
+		line = df.loc[df['topic_id'] == i, 'word']
+		line = line.str.lower()
+		line = sorted(list(set(line)))
+		line = ', '.join(line)
 		f.write(f'{i}, ' + line + '\n')
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument("--filename", type=str, default="topic_model/data/topic_priors_1.csv")
+	parser.add_argument("--filename", type=str, default="topic_model/data/topic_priors_sub_sex.csv")
 	args = parser.parse_args()
 	main(args)
 
