a36ffda91eb90b0293a7960116f2aa0fd56b143e
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-07-26 15:34:23 +0200

    refactor: clean up script for sampling data to be annotated

---
+++ scripts/generate_training_data.py
@@ -0,0 +1,54 @@
+'''
+Sample pages for annotation of classifier training data and/or OCR quality
+'''
+import pandas as pd
+import argparse
+import kblab
+from kblab.utils import flerge
+import random
+
+
+def sample_issues(archive, args):
+    random.seed(args.seed)
+    years = range(args.start, args.end)
+    dark_ids = []
+    for year in years:
+        # SWITCH TO datalab        
+        packages = archive.search({'label': args.label, 'meta.created': year})
+        if packages.n:
+            dark_ids.extend(random.sample(packages, args.n))
+    return dark_ids
+
+
+def sample_pages(archive, dark_ids):
+    random.seed(args.seed)
+    data = []
+    for dark_id in dark_ids:
+        p = archive.get(dark_id)
+        flerged_package = flerge(package = p, level = 'Text')
+        date = flerged_package[0]['meta']['created']
+        pages_urls = set([block['path'][2].get('@id') for block in flerged_package])
+        page_url = random.sample(list(pages_urls), 1)
+        data.append([date, page_url])
+    return pd.DataFrame(data, columns=['date', 'page_url'])
+
+
+def main(args):
+    with open(args.credentials, 'r') as f:
+	    credentials = f.read().rstrip('\n')
+    a = kblab.Archive('https://betalab.kb.se', auth=('demo', credentials))
+    dark_ids = sample_issues(a, args)
+    df = sample_pages(a, dark_ids)
+    df.to_csv('classifier/sample.csv', index=False)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--n", type=int, default=1, help="Number of pages per year")
+    parser.add_argument("--seed", type=int, default=123)
+    parser.add_argument("--start", type=int, default=1900)
+    parser.add_argument("--end", type=int, default=2022)
+    parser.add_argument("--credentials", type=str, help="Path to KB API credentials txt file") # /Users/robsa662/misc/keys/kb-api-credentials.txt
+    parser.add_argument("--label", type=str, help="API label with escaped whitespace") # DAGENS\ NYHETER
+    args = parser.parse_args()
+    main(args)
